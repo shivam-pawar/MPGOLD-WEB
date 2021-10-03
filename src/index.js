104e0fd0fdcb8f1c268b0b65230318bf15a64fec
shivam-pawar <shivampawar1038@gmail.com>
2021-10-03 20:55:26 +0530

    Updated App to Use as a Progressive Web App.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Login from "./components/Login";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<Login />, document.getElementById("root"));
+serviceWorkerRegistration.register();
